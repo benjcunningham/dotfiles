b4b1cb3fd8c3c3848e877114752ceba0b2fdf4fb
Ben Cunningham <benjamescunningham@gmail.com>
2020-07-12 11:53:52 -0400

    💡 Comment on main installer

--- install.sh
+++ install.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 if [ `uname` = "Darwin" ]; then
+
+    # macOS libraries
     scripts/brewinstall.sh
+
 elif [ `uname` = "Linux" ]; then
+
+    # Ubuntu libraries
     scripts/keysinstall.sh
     scripts/aptinstall.sh
+
 fi
 
+# Apps
 scripts/apps/miniconda.sh
 scripts/apps/docker.sh
 
+# Dotfiles
 scripts/dotbotinstall.sh
 
 echo "🌱 Done! This is a new beginning..."
