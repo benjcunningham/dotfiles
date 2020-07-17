236c081e0e1de5808e992f553874b924975399ab
Ben Cunningham <benjamescunningham@gmail.com>
2020-07-16 21:21:55 -0400

    🐳 Create separate installers for macOS / Ubuntu

--- scripts/apps/docker.sh
+++ scripts/apps/docker.sh
@@ -2,19 +2,26 @@
 
 echo "🐳 Installing Docker"
 
-sudo apt update
-sudo apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
+if [ `uname` = "Darwin" ]; then
+
+    # Docker Desktop for macOS
+    brew cask install docker
+
+elif [ `uname` = "Linux"]; then
+
+    # Docker CLI on Ubuntu
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       $(lsb_release -cs) \
+       stable"
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+else
+
+    echo "🙅 No installer available for your system (`uname`)"
+
+fi
 
 echo "✔️  Done installing Docker"
