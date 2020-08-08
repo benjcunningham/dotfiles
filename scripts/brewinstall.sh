fda3f88dfeecfa6c714be7cfc7f40aff999b0e5d
Ben Cunningham <benjamescunningham@gmail.com>
2020-08-07 21:31:14 -0400

    🐛 Fix Homebrew installer exit status issue

--- scripts/brewinstall.sh
+++ scripts/brewinstall.sh
@@ -4,7 +4,9 @@ echo "Installing macOS libraries with Homebrew"
 
 if which brew &> /dev/null; then
 
-    if bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" > /dev/null; then
+    BREW_STATUS="$(echo | bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)")"
+
+    if [ "$BREW_STATUS" -ne 0 ]; then
         echo "[ERROR] Failed to install Homebrew"
         exit 1
     else
