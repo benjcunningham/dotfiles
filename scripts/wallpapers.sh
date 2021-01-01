f753eb37196413c326eccc15bc9bd9e0252e9aae
Ben Cunningham <benjamescunningham@gmail.com>
2021-01-01 14:50:35 -0500

    🍱 Update wallpaper zip permalink

--- scripts/wallpapers.sh
+++ scripts/wallpapers.sh
@@ -7,12 +7,12 @@ source "scripts/util.sh"
 note "Installing wallpapers."
 
 WALLPAPERS_DIR="${HOME}/wallpapers"
-WALLPAPERS_URL="https://zip.imgur.com/0e1d1705d9c998e062d975fa5c0f336be0ef0924d35ff8e85c2da38031c5f265"
+WALLPAPERS_URL="https://imgur.com/a/i9tDIy3/zip"
 
 if [ ! -d "${WALLPAPERS_DIR}" ]; then
     mkdir "${WALLPAPERS_DIR}"
 fi
 
-curl -s -o "${WALLPAPERS_DIR}/archive.zip" "${WALLPAPERS_URL}"
+curl -sL -o "${WALLPAPERS_DIR}/archive.zip" "${WALLPAPERS_URL}"
 unzip -oq "${WALLPAPERS_DIR}/archive.zip" -d "${WALLPAPERS_DIR}"
 rm "${WALLPAPERS_DIR}/archive.zip"
