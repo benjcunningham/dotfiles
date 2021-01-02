a1d1e42daefde9ca266c31e5db214d37f6969cc1
Ben Cunningham <benjamescunningham@gmail.com>
2021-01-01 19:47:45 -0500

    🎨 Wrap var in braces, to be consistent

--- scripts/tpm.sh
+++ scripts/tpm.sh
@@ -6,7 +6,7 @@ source "scripts/util.sh"
 
 note "Installing TPM..."
 
-TPM_DIR="$HOME/.tmux/plugins/tpm"
+TPM_DIR="${HOME}/.tmux/plugins/tpm"
 
 if [ ! -d "${TPM_DIR}" ]; then
     git clone https://github.com/tmux-plugins/tpm "${TPM_DIR}"
