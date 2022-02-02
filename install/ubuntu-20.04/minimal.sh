7d1c9cda059e5369b81ce3b13bc34a3edaf27e14
Ben Cunningham <benjamescunningham@gmail.com>
2022-02-02 17:31:00 -0500

    Install OMZ, Dracula themes

--- install/ubuntu-20.04/minimal.sh
+++ install/ubuntu-20.04/minimal.sh
@@ -2,7 +2,8 @@
 
 # Ubuntu 20.04
 #
-# NOTE: This script should not be invoked directly, but rather through the main installer.
+# NOTE: This script should not be invoked directly, but rather through the main
+# installer.
 
 set -eo pipefail
 
@@ -12,15 +13,18 @@ source "install/utils.sh"
 note "Running Ubuntu 20.04 minimal installation"
 
 export DOTFILES_TARGET="ubuntu-20.04/minimal"
-DOTFILES_PERSONAL="${DOTFILES_PERSONAL:-0}"
-DOTFILES_WORK="${DOTFILES_WORK:-0}"
+export DOTFILES_PERSONAL="${DOTFILES_PERSONAL:-0}"
+export DOTFILES_WORK="${DOTFILES_WORK:-0}"
 
-note "Install software for minimal"
+note "Installing software for minimal"
+
 sudo apt-get update
 sudo apt-get install -y $(cat "pkglist/ubuntu-20.04")
 
+install/software/ohmyzsh.sh
 install/software/dotbot.sh dotbotconf/minimal.conf.yaml
 install/software/tpm.sh
 install/software/vundle.sh
+install/software/dracula.sh
 
 note "Completed Ubuntu 20.04 minimal installation"
