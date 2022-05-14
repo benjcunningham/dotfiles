c19e4bf1797ac3f475c6479bc14a3289358b5af9
Ben Cunningham <benjamescunningham@gmail.com>
2022-05-13 20:10:47 -0400

    Simplify setting gitconfig.local

---
+++ scripts/git-config.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eo pipefail
+
+source "scripts/util.sh"
+
+if [ "${DEBIAN_FRONTEND}" != "noninteractive" ]; then
+
+    note "Enter your Git credentials to populate ~/.gitconfig.local"
+
+    echo -n 'user.name: '
+    read user_name
+    git config --file ~/.gitconfig.local user.name "${user_name}"
+
+    echo -n 'user.email: '
+    read user_email
+    git config --file ~/.gitconfig.local user.email "${user_email}"
+
+fi
