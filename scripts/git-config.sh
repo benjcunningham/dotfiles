c5db2b5044e74c3d65d7b49647649a9d0710d034
Ben Cunningham <benjamescunningham@gmail.com>
2022-05-13 20:26:32 -0400

    Lint

--- scripts/git-config.sh
+++ scripts/git-config.sh
@@ -9,11 +9,11 @@ if [ "${DEBIAN_FRONTEND}" != "noninteractive" ]; then
     note "Enter your Git credentials to populate ~/.gitconfig.local"
 
     echo -n 'user.name: '
-    read user_name
+    read -r user_name
     git config --file ~/.gitconfig.local user.name "${user_name}"
 
     echo -n 'user.email: '
-    read user_email
+    read -r user_email
     git config --file ~/.gitconfig.local user.email "${user_email}"
 
 fi
